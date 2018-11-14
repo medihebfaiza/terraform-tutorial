617d5352a33706bc7c288dd95db263c475cdf9f9
Mohamed-Iheb FAIZA <mohamed-iheb.faiza@igtp3-mac05.isim.intra>
2018-11-14 18:38:39 +0100

    First week work

---
+++ aws_security_group.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
