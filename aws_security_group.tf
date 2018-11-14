1865a61f2f99de5a500a72d3dfecf77d4784b8c3
Mohamed-Iheb FAIZA <mohamed-iheb.faiza@igtp3-mac05.isim.intra>
2018-11-14 19:40:29 +0100

    Add : created outbound security group and added ssh key

--- aws_security_group.tf
+++ aws_security_group.tf
@@ -1,11 +1,24 @@
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
+resource "aws_security_group" "allow_ssh_in" {
+  name        = "allow_ssh_in"
+  description = "Allow inbound traffic"
   vpc_id      = "${aws_vpc.main.id}"
 
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_ssh_out" {
+  name        = "allow_ssh_out"
+  description = "Allow outbound traffic"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  egress {
+    from_port   = 3306
+    to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
